ff0fd4381343ef2859a0953ecacf5cc220501828
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-08 09:26:06 -0300

    feat: produce position and directions

---
+++ simulator/app/kafka/produce.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"simulator/app/route"
+	"simulator/infra/kafka"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, position := range positions {
+		kafka.Publish(position, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Microsecond * 500)
+	}
+}
