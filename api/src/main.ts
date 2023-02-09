b9ec99b602d3a71bde8657fb0843e398428e26c4
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-08 23:04:16 -0300

    feat: first api config

---
+++ api/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: process.env.KAFKA_CLIENT_ID,
+        brokers: [process.env.KAFKA_BROKER],
+      },
+      consumer: {
+        groupId: '2',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+}
+bootstrap();
