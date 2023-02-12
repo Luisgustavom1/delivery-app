5687768b4d55f8bc8add2249c010169c502e1bba
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-12 01:02:57 -0300

    feat: docker compose to kibana, es, kafka connect and control-center

--- front/src/ui/navbar/index.tsx
+++ front/src/ui/navbar/index.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
 return (
     <Center as='header' w='100%' bg='whiteAlpha.400' p={2} display='flex' alignItems='center' gap={3} color='RGBA(255, 255, 255, 0.64)'>
       <Car size={32} weight="regular" color='currentColor' />
-      <Heading as='h1' size='md' color='currentColor'>Delivery App</Heading>
+      <Heading as='h1' size='sm' color='currentColor'>Delivery App</Heading>
     </Center>
   );
 };
